8d85941a066c5c37a1b41732c9a862af04bb95f2
EmilN2S <emil.nazaryan@proton.me>
2026-09-26 11:08:51 +0400

    added other inline keyboards with emojis

--- src/keyboards/routes.py
+++ src/keyboards/routes.py
@@ -3,7 +3,11 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def main_keyboard() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Help", callback_data="help")],
+            [InlineKeyboardButton(text="➕ Add Task", callback_data="add_task")],
+            [InlineKeyboardButton(text="🗑️ Delete Task", callback_data="delete_task")],
+            [InlineKeyboardButton(text="📋 List Tasks", callback_data="list_tasks")],
+            [InlineKeyboardButton(text="📊 Change Task Status", callback_data="task_status")],
+            [InlineKeyboardButton(text="❓ Help", callback_data="help")],
         ],
         resize_keyboard=True,
     )
