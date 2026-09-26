2ac6e5e140d0ca68df953c392d9111e71882d5bb
EmilN2S <emil.nazaryan@proton.me>
2026-09-26 11:55:40 +0400

    added callback_query.answer to close answer

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -13,6 +13,7 @@ Here will be help information. (Coming soon!)
 @router.callback_query(lambda c: c.data == "help")
 async def help_callback_handler(callback_query: callback_query):
     await callback_query.message.answer(HELP_TEXT, reply_markup=main_keyboard())
+    await callback_query.answer()
 
 @router.message(Command("help"))
 async def help_handler(message: Message):
