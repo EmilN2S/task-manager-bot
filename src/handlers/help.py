4336a07066d141034fe8a3c1a1f0a564e5fe7ca5
EmilN2S <emil.nazaryan@proton.me>
2026-09-26 11:54:14 +0400

    added type callback_query to help callback

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, callback_query
 
 from keyboards.inline import main_keyboard
 
@@ -11,7 +11,7 @@ Here will be help information. (Coming soon!)
 """
 
 @router.callback_query(lambda c: c.data == "help")
-async def help_callback_handler(callback_query):
+async def help_callback_handler(callback_query: callback_query):
     await callback_query.message.answer(HELP_TEXT, reply_markup=main_keyboard())
 
 @router.message(Command("help"))
